1c818dd47d7e418e49951f2deffac39fea885b9b
SEEKASIA\andrewkhaw <andrewkhaw@seekasia.com>
2017-12-12 12:27:58 +1100

    first commit

---
+++ src/config/data.js
@@ -0,0 +1,13 @@
+export default {
+  ingredients: [egg, avacado, mushroom, bread],
+  menu: [
+    1: {
+      ingredients: ['egg', 'bread', 'mushroom'],
+      image: '...'
+    },
+    2: {
+      ingredients: ['avacado', 'bread'],
+      image: '...'
+    }
+  ]
+};
